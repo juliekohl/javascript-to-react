3a7c13b98d64c42c302b6bbcfc9282ba16fb3790
Julie Kohl <juliana.cochenski@gmail.com>
2021-07-14 17:59:25 -0300

    Finish the BoardGame object.

--- src/components/objects/BoardGame/index.js
+++ src/components/objects/BoardGame/index.js
@@ -1,12 +1,14 @@
+import "./style.css";
 import CardGame from "../../CardGame";
 
 function BoardGame(amountCards) {
   const $htmlCardGame = CardGame();
-  const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
+  const $htmlContent = $htmlCardGame.repeat(amountCards);
 
-  console.log($htmlBoardGame);
-
-  return $htmlBoardGame;
+  return `
+    <section class="board-game">
+      ${$htmlContent}
+    </section>`;
 }
 
 export default BoardGame;
