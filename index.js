f9d82da3a305251840cfd6169ffca989a38a6ee5
Julie Kohl <juliana.cochenski@gmail.com>
2021-07-14 17:41:16 -0300

    Create new BoardGame object without CSS WIP.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
-import CardGame from "./src/components/CardGame";
 import TextGame from "./src/components/TextGame";
+import BoardGame from "./src/components/objects/BoardGame";
 
 const $root = document.querySelector("#root");
-
-// const $htmlCardGame = CardGame() + CardGame(); // se quiser usar mais uma vez.
-const $htmlCardGame = CardGame();
-console.log($htmlCardGame);
+const $htmlBoardGame = BoardGame(6);
 
 const htmlTextGame = TextGame();
 console.log(htmlTextGame);
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame + $htmlCardGame + $htmlCardGame + $htmlCardGame + $htmlCardGame + $htmlCardGame); // se queser usar mais uma vez, mudando a estrateja.
-// $root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame); 
